f868a6d58248a401d5b64ed677717cc6a58a2002
Rushifaaa <jakubgencer@protonmail.com>
2021-05-20 09:42:58 +0200

    Add API class

---
+++ BookStore/BookStore/Services/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BookStore.Services
+{
+    public class API
+    {
+        static public API shared = new API();
+
+        private HttpClient APIClient;
+
+        private API()
+        {
+            APIClient = new HttpClient();
+            APIClient.BaseAddress = new Uri("localhost:");
+        }
+    }
+}
