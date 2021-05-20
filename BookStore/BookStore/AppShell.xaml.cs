7dae93c2a356f6719c65c5ba3adad31d7ce14569
Rushifaaa <jakubgencer@protonmail.com>
2021-05-20 09:22:32 +0200

    Add BooksPage

--- BookStore/BookStore/AppShell.xaml.cs
+++ BookStore/BookStore/AppShell.xaml.cs
@@ -11,7 +11,7 @@ namespace BookStore
         public AppShell()
         {
             InitializeComponent();
-           // Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(BooksPage), typeof(BooksPage));
         }
 
     }
