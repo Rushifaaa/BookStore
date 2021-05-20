4a19cba3d4069b1a9c549e1093091b8ddf296e68
Rushifaaa <jakubgencer@protonmail.com>
2021-05-20 09:22:48 +0200

    Add Book Model

---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsAvailable { get; set; }
+        public string ISBN { get; set; }
+    }
+}
